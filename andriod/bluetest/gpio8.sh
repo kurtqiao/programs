99157e0edb95f8a4926277c504928d6aa5ae8965
kurtqiao <kurtqqh@gmail.com>
2015-06-27 17:32:00 +0800

    add gpio8 setup script, bluetooth SPP loopback, update readme file

---
+++ andriod/bluetest/gpio8.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo 49 > /sys/class/gpio/export
+echo 214 > /sys/class/gpio/export
+echo 256 > /sys/class/gpio/export
+echo low > /sys/class/gpio/gpio214/direction
+echo high > /sys/class/gpio/gpio256/direction
+echo high > /sys/class/gpio/gpio214/direction
+echo high > /sys/class/gpio/gpio214/direction
+echo out > /sys/class/gpio/gpio49/direction
+
+#blow script use to blink GPIO8 LED
+#while :
+#do
+# echo 1 > /sys/class/gpio/gpio49/value
+# sleep 1
+# echo 0 > /sys/class/gpio/gpio49/value
+# sleep 1
+#done
